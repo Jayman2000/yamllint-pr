e860a416df231419fc8b61e0483559a5418840ab
Jason Yundt <jason@jasonyundt.email>
2024-01-03 11:50:42 -0500

    tests: Restore stdout and stderr
    Before this commit, test_run_default_format_output_in_tty() changed the
    values of sys.stdout and sys.stderr, but it would never change them
    back. This commit makes sure that they get changed back.
    
    At the moment, this commit doesn’t make a user-visible difference. A
    future commit will add a new test named test_ignored_from_file_with_multiple_encodings().
    That new test requires stdout and stderr to be restored, or else it will
    fail.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -517,6 +517,7 @@ class CommandLineTestCase(unittest.TestCase):
         path = os.path.join(self.wd, 'a.yaml')
 
         # Create a pseudo-TTY and redirect stdout to it
+        old_stdout, old_stderr = sys.stdout, sys.stderr
         master, slave = pty.openpty()
         sys.stdout = sys.stderr = os.fdopen(slave, 'w')
 
@@ -536,6 +537,7 @@ class CommandLineTestCase(unittest.TestCase):
         sys.stdout.close()
         sys.stderr.close()
         output.close()
+        sys.stdout, sys.stderr = old_stdout, old_stderr
 
         self.assertEqual(out, (
             f'\033[4m{path}\033[0m\n'
